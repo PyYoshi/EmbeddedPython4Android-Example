000653acb272398d232d712ebe3b983a43723572
PyYoshi <myoshi321go+github@gmail.com>
2012-11-16 23:04:18 +0900

    pythonをビルドするようにパス追加

--- app_src/jni/Android.mk
+++ app_src/jni/Android.mk
@@ -3,6 +3,7 @@ subdir_makefiles := \
     $(LOCAL_PATH)/libcrypt/Android.mk \
     $(LOCAL_PATH)/sqlite3/Android.mk \
     $(LOCAL_PATH)/openssl/Android.mk \
-    $(LOCAL_PATH)/load_python/Android.mk
+    $(LOCAL_PATH)/load_python/Android.mk \
+    $(LOCAL_PATH)/Python-arm/Android.mk
 
 include $(subdir_makefiles)
