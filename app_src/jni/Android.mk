660b56ae9a3a130895717858dc1f9c092f8fe242
PyYoshi <myoshi321go+github@gmail.com>
2012-11-27 00:21:56 +0900

    added liffi

--- app_src/jni/Android.mk
+++ app_src/jni/Android.mk
@@ -6,6 +6,7 @@ subdir_makefiles := \
     $(LOCAL_PATH)/Python-arm/Android.mk \
     $(LOCAL_PATH)/load_python/Android.mk \
     $(LOCAL_PATH)/bzip2/Android.mk \
+    $(LOCAL_PATH)/libffi_android/Android.mk \
     $(LOCAL_PATH)/expat_android/Android.mk
 
 include $(subdir_makefiles)
