69cd66d52b935ccf89641cf90debb5fd32958e29
PyYoshi <myoshi321go+github@gmail.com>
2012-11-26 22:36:02 +0900

    added expat

--- app_src/jni/Android.mk
+++ app_src/jni/Android.mk
@@ -3,7 +3,8 @@ subdir_makefiles := \
     $(LOCAL_PATH)/libcrypt/Android.mk \
     $(LOCAL_PATH)/sqlite3/Android.mk \
     $(LOCAL_PATH)/openssl/Android.mk \
+    $(LOCAL_PATH)/Python-arm/Android.mk \
     $(LOCAL_PATH)/load_python/Android.mk \
-    $(LOCAL_PATH)/Python-arm/Android.mk
+    $(LOCAL_PATH)/expat_android/Android.mk
 
 include $(subdir_makefiles)
